47774eb41eff495802d759b1b89fb296f7ea39e4
Robert Gunze <robertgunze@gmail.com>
2013-07-04 12:16:41 +0300

    Enhanced look and feel on home page

--- protected/views/site/_lookup.php
+++ protected/views/site/_lookup.php
@@ -11,10 +11,14 @@
       );
 ?>
 <div class="row" style="float:left">
-         <?php echo $form->error($lookup,'domain');?>
+    <div class="input-prepend">
+         <span class="add-on" style="height:35px;"></span>
          <?php echo $form->textField($lookup,'domain',array('style'=>'width:300px;height:35px;font-size:20px;')); ?>
+         
+    </div>
          <?php echo $form->dropDownList($lookup,'sld',  IkelRegistry::getSldList(),array('style'=>'width:80px;height:45px;font-size:20px;'));?>
          <?php echo CHtml::submitButton('Lookup',array('class'=>'btn btn-large btn-primary','style'=>'margin-top:-10px;')); ?>
+         <?php echo $form->error($lookup,'domain');?>
 </div>
 
 <?php $this->endWidget();?>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -15,17 +15,30 @@ $this->pageTitle=Yii::app()->name;
                                 );
                     ?>
      <?php endif;?>
-   <p> <?php $this->renderPartial('_lookup',array('lookup'=>$lookup));?></p>
+     <?php $this->renderPartial('_lookup',array('lookup'=>$lookup));?>
     
 
 <br /><br />
-<p>EPP is the Extensible Provisioning Protocol. EPP (defined in RFC 5730 and subsequent documents) is an application layer client-server protocol for the provisioning and management of objects stored in a shared central repository.</p> 
-<p>Specified in XML, the protocol defines generic object management operations and an extensible framework that maps protocol operations to objects. As of writing, its only well-developed application is the provisioning of Internet domain names, hosts, and related contact details.
-
-	<div class="row buttons" style="float:right">
+<div class="well">
+    <img style="width:170px; height:170px" src="<?php echo Yii::app()->request->baseUrl;?>/images/DomExpProt.png" class="img-polaroid">
+    <img style="width:170px; height:170px" src="<?php echo Yii::app()->request->baseUrl;?>/images/domain-names.jpg" class="img-polaroid">
+    <img style="width:170px; height:170px" src="<?php echo Yii::app()->request->baseUrl;?>/images/domain-names.jpg" class="img-polaroid">
+    <img style="width:170px; height:170px" src="<?php echo Yii::app()->request->baseUrl;?>/images/icann.jpg" class="img-polaroid">
+</div>
+<div class="well">
+    <p>EPP is the Extensible Provisioning Protocol. EPP (defined in RFC 5730 and subsequent documents) is an application layer client-server protocol for the provisioning and management of objects stored in a shared central repository.</p> 
+</div>
+ <div class="row buttons" style="float:right">
 		<?php echo CHtml::linkButton('Register for an API key',array('class'=>'btn btn-large btn-primary')); ?>
 	        <?php echo CHtml::linkButton('Login to buy .tz domain',array('class'=>'btn btn-large btn-primary')); 
                 ?>
-        </div>
+ </div>
+
+<br/><br />
+<div class="well">
+   
+</div>
+
+	
 
 </div>
